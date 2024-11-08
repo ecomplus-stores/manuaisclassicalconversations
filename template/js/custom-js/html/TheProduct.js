139c0e809c70ebeb4a4d0259b1a0ac4cb52d1b21
vitorrgg <vitorrggm@gmail.com>
2024-11-07 23:28:44 -0300

    fixing how theproduct component behave with segmentation

--- template/js/custom-js/html/TheProduct.js
+++ template/js/custom-js/html/TheProduct.js
@@ -214,10 +214,12 @@ import {
       }, 
 
       isLogged () {
+        const hasGroupSpec = this.body.specifications && this.body.specifications['grupo'] && this.body.specifications['grupo'].length
+        if (!hasGroupSpec) return true
+        
         const group = window.sessionStorage.getItem('isGroup')
-        const hasSpec = this.body.specifications && this.body.specifications['grupo'] && this.body.specifications['grupo'].length && this.body.specifications['grupo'][0].text === group
-        return ecomPassport.checkAuthorization() && hasSpec
-        //return true
+        console.log("group", group)
+        return ecomPassport.checkAuthorization() && this.body.specifications['grupo'][0].text === group
       },
   
       isVariationInStock () {
